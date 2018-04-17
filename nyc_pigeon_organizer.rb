0d824f8c9e14c1e9768e37df63fcc0e895bf25c2
Saige Leslie <saigeleslie@gmail.com>
2018-04-17 00:34:08 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  color = data[:color].keys
+  
+  keys = value.values.flatten.uniq
+  keys.each do |x|
+    pigeon_list[x] = {}
+  end
+  
+  data.each do |key, value|
+    color.each do |x|
+      if value[x].include?(key)
+        binding.pry
+      end
+    end
+  end
+  
+  data.each do |key, value|
+    value.each do |k1, v1|
+      pigeon_list.each do |k2, v2|
+        if v1.include?(k2) 
+          pigeon_list[k2][key] = [k1.to_s]
+          if key == :color
+            pigeon_list[k2][key] = color
+            #binding.pry
+          end
+        end
+      end
+    end
+  end
+  
+  
+  pigeon_list
+  binding.pry
 end
