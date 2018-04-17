a24a861900faaef59ac0e4de94331d3c7ba3262d
Saige Leslie <saigeleslie@gmail.com>
2018-04-17 01:22:11 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,7 +14,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |k1, v1|
       pigeon_list.each do |k2, v2|
-        if v1.include?(k2) 
+        if v1.include?(k2) && key != :color
           pigeon_list[k2][key] = [k1.to_s]
         end
       end
